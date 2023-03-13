cd1d66e4cf2c67d09925349709cfd7385a780d77
Japheth Yang <japheth720@gmail.com>
2023-03-12 21:02:57 -0500

    finished loop assignent

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -47,6 +47,10 @@ let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
 // 2.a. TODO: Write a for of loop to console.log each star in the 'stars' array
 console.log('Some stars:');
 
+for (let galaxy of stars) {
+  console.log(galaxy);
+
+}
 
 // 3. While loops  
 console.log('---- 3. While loop ----');
@@ -54,8 +58,36 @@ console.log('---- 3. While loop ----');
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
 console.log('Some stars using while:');
 
+let h = 0;
+let planets = "Stars include: ";
+
+while (stars[h]) {
+  planets +=stars[h];
+  h++
+
+  console.log(planets);
+}
+
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
 
+let index = 0;
+const max = 6;
+//while loop
+while(index < max){
+  console.log('in the while loop:', index);
+  index++;
+}
+
+
+
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
 console.log('count backwards from 10 to 5');
+
+let windex = 10;
+const wax = 4;
+//while loop
+while(windex > wax){
+  console.log('in the while loop:', windex);
+  windex--;
+}
